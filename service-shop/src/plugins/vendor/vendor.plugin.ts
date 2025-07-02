3e74c80b19ee1c38601f48786b7d0f1daa34162c
gtlegend <yusuf_ar@live.com>
2025-07-02 00:34:15 -0400

    updated vendor.plugin.ts to update adminAPI schema using gql as wasnt wired to the API so custom mutation was not avalaible.

--- service-shop/src/plugins/vendor/vendor.plugin.ts
+++ service-shop/src/plugins/vendor/vendor.plugin.ts
@@ -1,11 +1,47 @@
 import { PluginCommonModule, VendurePlugin } from '@vendure/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import gql from 'graphql-tag';
 import { Vendor } from './vendor.entity';
 import { VendorResolver } from './graphql/vendor.resolver';
 
 @VendurePlugin({
   imports: [PluginCommonModule, TypeOrmModule.forFeature([Vendor])],
-  entities: [Vendor], //Registers your Vendor entity with the DB
+  entities: [Vendor],
   providers: [VendorResolver],
+  adminApiExtensions: {
+    schema: gql`
+      extend type Query {
+        vendors: [Vendor!]!
+      }
+
+      extend type Mutation {
+        createVendor(input: CreateVendorInput!): Vendor!
+      }
+
+      input CreateVendorInput {
+        name: String!
+        contactNumber: String!
+        address: String!
+        gpsLat: Float!
+        gpsLng: Float!
+        isOpen: Boolean!
+        businessType: String!
+        deliveryAvailable: Boolean!
+      }
+
+      type Vendor {
+        id: ID!
+        name: String!
+        contactNumber: String!
+        address: String!
+        gpsLat: Float!
+        gpsLng: Float!
+        isOpen: Boolean!
+        businessType: String!
+        deliveryAvailable: Boolean!
+      }
+    `,
+    resolvers: [VendorResolver],
+  },
 })
 export class VendorPlugin {}
